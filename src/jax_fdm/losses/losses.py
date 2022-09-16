76e682c8c35a84c287dba3dd333fbe0d12f6999a
Rafael Pastrana <arpj@princeton.edu>
2022-09-15 20:11:53 -0400

    [Losses] Jitted loss `__call__`

--- src/jax_fdm/losses/losses.py
+++ src/jax_fdm/losses/losses.py
@@ -1,4 +1,8 @@
+from functools import partial
+
 import jax.numpy as jnp
+
+from jax import jit
 
 from jax_fdm.goals import goals_state
 
@@ -19,10 +23,12 @@ class LossTerm:
             self._name = self.__class__.__name__
         return self._name
 
+    @partial(jit, static_argnums=(0, 2))
     def __call__(self, eqstate, model):
         gstate = self.goals_state(eqstate, model)
         return self.alpha * self.loss(gstate)
 
+    @partial(jit, static_argnums=(0, 2))
     def goals_state(self, eqstate, model):
         return goals_state(self.goals, eqstate, model)
 
@@ -41,16 +47,18 @@ class SquaredError(LossTerm):
     Measures the distance between the current and the target value of a goal.
     """
     @staticmethod
+    @jit
     def loss(gstate):
         return jnp.sum(gstate.weight * jnp.square(gstate.prediction - gstate.target))
 
 
 class MeanSquaredError(LossTerm):
     """
-    The seminal mean squared error loss.
+    The seminal mean squared error.
     Average out all errors because no single error is important enough.
     """
     @staticmethod
+    @jit
     def loss(gstate):
         return jnp.mean(gstate.weight * jnp.square(gstate.prediction - gstate.target))
 
@@ -60,6 +68,7 @@ class PredictionError(LossTerm):
     You lose when you predict too much of something.
     """
     @staticmethod
+    @jit
     def loss(gstate):
         return jnp.sum(gstate.prediction)
 
@@ -73,11 +82,12 @@ class Loss:
         self.loss_terms = args
         self._name = name
 
+    @partial(jit, static_argnums=(0, 2))
     def __call__(self, q, model):
         eqstate = model(q)
         error = 0.0
         for loss in self.loss_terms:
-            error += loss(eqstate, model)
+            error = error + loss(eqstate, model)
         return error
 
     @property
