b45a708b824fb916791d651de68fd017a7344fc2
Rafael Pastrana <arpj@princeton.edu>
2022-09-19 15:34:47 -0400

    Cleaned up

--- src/jax_fdm/goals/networkgoal/__init__.py
+++ src/jax_fdm/goals/networkgoal/__init__.py
@@ -1,7 +1,7 @@
-# from .<module> import *
 from .networkgoal import *  # noqa F403
 from .loadpathgoal import *  # noqa F403
 from .directiongoal import *  # noqa F403
 from .lengthgoal import *  # noqa F403
+
 
 __all__ = [name for name in dir() if not name.startswith('_')]
