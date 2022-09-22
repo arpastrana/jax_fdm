6a05a4e0adadecf9ddf484aef7afd64c4d0fad80
Rafael Pastrana <arpj@princeton.edu>
2022-09-22 14:53:27 -0400

    [Goals] Renamed `gstate.target` as `gstate.goal`

--- src/jax_fdm/goals/state.py
+++ src/jax_fdm/goals/state.py
@@ -1,9 +1,10 @@
 from typing import NamedTuple
 
+import numpy as np
 import jax.numpy as jnp
 
 
 class GoalState(NamedTuple):
+    goal: np.ndarray
+    weight: np.ndarray
     prediction: jnp.ndarray
-    target: jnp.ndarray
-    weight: jnp.ndarray
