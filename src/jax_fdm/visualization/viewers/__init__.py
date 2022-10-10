54fd69be38913872ab6f59b3b311323d0ea45a93
Rafael Pastrana <arpj@princeton.edu>
2022-10-10 16:05:44 -0400

    [Viz] Moved  install check to

--- src/jax_fdm/visualization/viewers/__init__.py
+++ src/jax_fdm/visualization/viewers/__init__.py
@@ -1,6 +1,15 @@
-from .network import *  # noqa F403
-from .viewer import *  # noqa F403
-from .register import register_artists
+def is_compasview_installed():
+    try:
+        import compas_view2  # noqa F401
+    except ImportError:
+        print("Compas View 2 is not installed and thus jax_fdm.visualization won't be available. Skipping import")
+        return False
+    return True
 
 
-register_artists()
+if is_compasview_installed():
+    from .network import *  # noqa F403
+    from .viewer import *  # noqa F403
+    from .register import register_artists
+
+    register_artists()
