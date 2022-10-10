399577001860da176325597a25920763696e0877
Rafael Pastrana <arpj@princeton.edu>
2022-10-10 16:20:51 -0400

    test

--- src/jax_fdm/visualization/viewers/__init__.py
+++ src/jax_fdm/visualization/viewers/__init__.py
@@ -1,17 +1,8 @@
-def is_compasview_installed():
-    try:
-        import compas_view2  # noqa F401
-    except ImportError:
-        print("Compas View 2 is not installed and thus jax_fdm.visualization won't be available. Skipping import")
-        return False
-    return True
+from .network import *  # noqa F403
+from .viewer import *  # noqa F403
+from .register import register_artists
 
 
-if is_compasview_installed():
-    from .network import *  # noqa F403
-    from .viewer import *  # noqa F403
-    from .register import register_artists
-
-    register_artists()
+register_artists()
 
 __all__ = [name for name in dir() if not name.startswith('_')]
