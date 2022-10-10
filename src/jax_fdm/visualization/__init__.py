a96ef9744d88a4bef1d08b38415e836275e7b8d1
Rafael Pastrana <arpj@princeton.edu>
2022-10-10 15:45:20 -0400

    [Viz] Skip import of `jax_fdm.viewers.visualization` if `compas_view2` is not installed

--- src/jax_fdm/visualization/__init__.py
+++ src/jax_fdm/visualization/__init__.py
@@ -1,4 +1,9 @@
 from .artists import *  # noqa F403
-from .viewers import *  # noqa F403
-from .register import *  # noqa F403
 from .plotters import *  # noqa F403
+
+try:
+    from .viewers import *  # noqa F403
+    from .register import *  # noqa F403
+except (ImportError, ModuleNotFoundError):
+    print("Compas View 2 is not installed and thus jax_fdm.visualization won't be available. Skipping import")
+    pass
