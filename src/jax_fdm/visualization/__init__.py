10dd698ff09d973ef42ec0092b8e079ce047a198
Rafael Pastrana <arpj@princeton.edu>
2022-10-10 22:06:52 -0400

    [Viz] Added  to exception

--- src/jax_fdm/visualization/__init__.py
+++ src/jax_fdm/visualization/__init__.py
@@ -5,7 +5,7 @@ from .plotters import *  # noqa F403
 def is_compasview_installed():
     try:
         import compas_view2  # noqa F401
-    except ImportError:
+    except (ImportError, ModuleNotFoundError):
         print("Compas View 2 is not installed and thus jax_fdm.visualization won't be available. Skipping import")
         return False
     return True
