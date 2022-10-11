124af5f3686cde77d25e219677f472c508ecd3f3
Rafael Pastrana <arpj@princeton.edu>
2022-10-10 22:23:05 -0400

    [Actions] Switched to workflow build with conda

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -61,7 +60,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=requirements,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     extras_require=optional_requirements,
     entry_points={
         "console_scripts": [],
