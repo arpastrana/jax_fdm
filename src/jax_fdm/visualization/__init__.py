f22f8ad01624f87bda08c67491b5ec6db865d2a0
Rafael Pastrana <arpj@princeton.edu>
2022-10-11 22:24:28 -0400

    [Viz] Removed `is_compas_view_installed()` from `__init__` file

--- src/jax_fdm/visualization/__init__.py
+++ src/jax_fdm/visualization/__init__.py
@@ -1,18 +1,6 @@
 from .artists import *  # noqa F403
 from .plotters import *  # noqa F403
-
-
-def is_compasview_installed():
-    try:
-        import compas_view2  # noqa F401
-    except (ImportError, ModuleNotFoundError):
-        print("Compas View 2 is not installed and thus jax_fdm.visualization won't be available. Skipping import")
-        return False
-    return True
-
-
-if is_compasview_installed():
-    from .viewers import *  # noqa F403
+from .viewers import *  # noqa F403
 
 
 __all__ = [name for name in dir() if not name.startswith('_')]
