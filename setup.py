1f5b5af1031fd19b2a039949b124cfd16b735965
Rafael Pastrana <arpj@princeton.edu>
2022-10-17 17:12:05 -0400

    [Setup] Added support for `python==3.7`

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -60,7 +61,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=requirements,
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     extras_require=optional_requirements,
     entry_points={
         "console_scripts": [],
