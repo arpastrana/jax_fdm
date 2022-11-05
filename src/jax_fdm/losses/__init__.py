2c01b81e9885917960ea3be3c552f1b08bcf60a6
Rafael Pastrana <arpj@princeton.edu>
2022-11-05 16:54:49 -0400

    [Losses] Renamed `losses.py` to `loss.py`

--- src/jax_fdm/losses/__init__.py
+++ src/jax_fdm/losses/__init__.py
@@ -1,6 +1,6 @@
 from .errors import *  # noqa F403
 from .regularizers import *  # noqa F403
-from .losses import *  # noqa F403
+from .loss import *  # noqa F403
 
 
 __all__ = [name for name in dir() if not name.startswith('_')]
