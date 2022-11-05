11dcc47c72f0af861c29b63ead0d182955783521
Rafael Pastrana <arpj@princeton.edu>
2022-11-05 17:01:12 -0400

    [Examples] Added `arch_loadpath.py` example

---
+++ examples/arch/arch_loadpath.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from jax_fdm.datastructures import FDNetwork
+from jax_fdm.equilibrium import fdm
+from jax_fdm.equilibrium import constrained_fdm
+from jax_fdm.optimization import SLSQP
+from jax_fdm.constraints import EdgeLengthConstraint
+from jax_fdm.goals import NetworkLoadPathGoal
+from jax_fdm.losses import PredictionError
+from jax_fdm.losses import Loss
+from jax_fdm.visualization import Viewer
+
+
+network = FDNetwork.from_json("data/json/arch.json")
+network.edges_forcedensities(q=-1.0)
+network.nodes_supports(keys=[node for node in network.nodes() if network.is_leaf(node)])
+network.nodes_loads([0.0, 0.0, -0.3])
+
+f_network = fdm(network)
+
+loss = Loss(PredictionError(goals=[NetworkLoadPathGoal()]))
+constraints = [EdgeLengthConstraint(edge, 0.75, 1.0) for edge in network.edges()]
+c_network = constrained_fdm(network, loss=loss, optimizer=SLSQP(), constraints=constraints)
+
+viewer = Viewer(width=1600, height=900)
+viewer.add(c_network)
+viewer.add(f_network, as_wireframe=True)
+viewer.show()
