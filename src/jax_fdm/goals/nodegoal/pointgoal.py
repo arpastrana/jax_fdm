39a34879e4f482872fb050b2d34533f9d3207234
Rafael Pastrana <arpj@princeton.edu>
2022-11-09 18:12:30 -0500

    [Goals] Created `NodePointZGoal`

--- src/jax_fdm/goals/nodegoal/pointgoal.py
+++ src/jax_fdm/goals/nodegoal/pointgoal.py
@@ -1,3 +1,6 @@
+import jax.numpy as jnp
+
+from jax_fdm.goals import ScalarGoal
 from jax_fdm.goals import VectorGoal
 
 from jax_fdm.goals.nodegoal import NodeGoal
@@ -13,3 +16,16 @@ class NodePointGoal(VectorGoal, NodeGoal):
         The current xyz coordinates of the node in a network.
         """
         return eq_state.xyz[index, :]
+
+
+class NodePointZGoal(ScalarGoal, NodeGoal):
+    """
+    Make a node of a network to reach target xyz coordinates.
+    """
+    @staticmethod
+    def prediction(eq_state, index):
+        """
+        The current xyz coordinates of the node in a network.
+        """
+        # return jnp.reshape(eq_state.xyz[index, 2], (-1, 1))
+        return eq_state.xyz[index, 2:]
