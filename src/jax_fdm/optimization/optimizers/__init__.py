6851fe1daee3226a3f04ce57205d96941c56f8eb
Rafael Pastrana <arpj@princeton.edu>
2022-11-19 11:28:02 -0500

    [Reqs] Removed `cyipopt` from pypi dependencies

--- src/jax_fdm/optimization/optimizers/__init__.py
+++ src/jax_fdm/optimization/optimizers/__init__.py
@@ -2,6 +2,10 @@ from .optimizer import *  # noqa F403
 from .constrained import *  # noqa F403
 from .second_order import *  # noqa F403
 from .optimizers import *  # noqa F403
-from .ipopt import *  # noqa F403
+
+try:
+    from .ipopt import *  # noqa F403
+except ImportError:
+    pass
 
 __all__ = [name for name in dir() if not name.startswith('_')]
