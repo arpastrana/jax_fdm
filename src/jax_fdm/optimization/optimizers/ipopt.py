03865f0013b2fd891a1d25d9fd11ec557853437e
Rafael Pastrana <arpj@princeton.edu>
2022-11-19 10:20:51 -0500

    [Optimization] WIP updates in `IPOPT` wrapper

--- src/jax_fdm/optimization/optimizers/ipopt.py
+++ src/jax_fdm/optimization/optimizers/ipopt.py
@@ -8,7 +8,7 @@ from jax_fdm.optimization.optimizers import ConstrainedOptimizer
 from functools import partial
 
 from jax import jit
-from jax import jacfwd, jacrev, hessian, jvp, grad, vmap
+from jax import jacfwd, jacrev, hessian, jvp, grad, vmap, vjp
 
 import jax.numpy as jnp
 
@@ -32,7 +32,7 @@ class IPOPT(ConstrainedOptimizer, SecondOrderOptimizer):
     -----
     Ipopt expects that the loss and constraint functions are twice differentiable.
     """
-    def __init__(self, acc_tol=1e-9, disp=5, **kwargs):
+    def __init__(self, acc_tol=1e-9, disp=1, **kwargs):
         super().__init__(name="IPOPT", disp=disp, **kwargs)
         self.acceptable_tol = acc_tol
 
@@ -67,7 +67,7 @@ class IPOPT(ConstrainedOptimizer, SecondOrderOptimizer):
 
         print(f"Constraints: {len(constraints)}")
         constraints = self.collect_constraints(constraints)
-        print(f"Constraint colections: {len(constraints)}")
+        print(f"\tConstraint colections: {len(constraints)}")
 
         clist = []
         for constraint in constraints:
@@ -75,80 +75,91 @@ class IPOPT(ConstrainedOptimizer, SecondOrderOptimizer):
             # initialize constraint
             constraint.init(model)
 
-            # fun = partial(constraint, model=model)
-            # cfuns = [partial(_constraint_eq, constraint=constraint, model=model)]
-            cfuns = []
+            # select constraint functions
+            funs = []
+            if jnp.allclose(constraint.bound_low, constraint.bound_up):
+                ctype = "eq"
+                funs.append(self.constraint_eq)
+                print("\tAdding one equality constraint")
+            else:
+                ctype = "ineq"
+                if not jnp.allclose(constraint.bound_low, -jnp.inf):
+                    print("\tAdding bound low inequality constraint")
+                    funs.append(self.constraint_ineq_low)
+                if not jnp.allclose(constraint.bound_up, jnp.inf):
+                    print("\tAdding bound high inequality constraint")
+                    funs.append(self.constraint_ineq_up)
 
-            # equality constraint
-            # if constraint.bound_low == constraint.bound_up:
-            #     ctype = "eq"
-            #     print("Adding one equality constraint", constraint.bound_low)
-            #     fun = partial(self.constraint_eq, constraint=constraint, model=model)
-            #     _ = fun(params_opt)  # warm start
-            #     cfuns.append(fun)
+            if len(funs) == 0:
+                print("\tNo constraints were processed. Check the constraint bounds!")
+                return clist
 
-            # else:
-            #     ctype = "ineq"
-            #     if constraint.bound_low != -jnp.inf:
-            #         print("Adding lower bound constraint", constraint.bound_low)
-            #         fun = partial(self.constraint_ineq_low, constraint=constraint, model=model)
-            #         _ = fun(params_opt)  # warm start
-            #         cfuns.append(fun)
-            #     if constraint.bound_up != jnp.inf:
-            #         print("Adding upper bound constraint", constraint.bound_up)
-            #         fun = partial(self.constraint_ineq_up, constraint=constraint, model=model)
-            #         _ = fun(params_opt)  # warm start
-            #         cfuns.append(fun)
-
-            # ctype = "eq"
-            # print("Adding one equality constraint")
-            # fun = partial(self.constraint_eq, constraint=constraint, model=model)
-            # _ = fun(params_opt)  # warm start
-            # cfuns.append(fun)
-
-            ctype = "ineq"
-            for method in (self.constraint_ineq_low, self.constraint_ineq_up):
-                cfun = partial(method, constraint=constraint, model=model)
-                cfuns.append(cfun)
-
-            for cfun in cfuns:
+            for fun in funs:
 
                 cdict = {}
 
+                cfun = partial(fun, constraint=constraint, model=model)
                 jac = jit(jacfwd(cfun))
+                hvp = jit(Partial(self.hvp, f=cfun))
+
+                # warm start
+                c = cfun(params_opt)
+                j = jac(params_opt)
+                h = hvp(params_opt, jnp.ones_like(c))
+
 
                 # hessvp = jit(lambda x, v: self.hvp(cfun, (x, ), (v, )))  # hessian vector-product
                 # hess = hessian(partial(self.dot_xv, cfun=cfun))
                 # hess = jit(grad(lambda x, v: jnp.vdot(grad(cfun)(x), v))(x))
                 # hess = hessian(cfun)
-                hvp = jit(Partial(self.hvp, f=cfun))
-                # hvp = jit(Partial(self.hvp2, f=cfun))
+                # hvp = jit(Partial(self.hvp3, f=cfun))
+                # hess = jit(jacfwd(jacfwd(cfun)))
 
                 # hess = jit(lambda x, v: jvp(grad(cfun), (x, ), (v, )))
                 # hess = jit(partial(self.hvp2, f=cfun))
 
-                c = cfun(params_opt)  # warm start
-                j = jac(params_opt)  # warm start
-                h = hvp(params_opt, jnp.ones_like(c))  # warm start
-                assert h.shape == (params_opt.size, params_opt.size), f"We want: {(params_opt.size, params_opt.size)}"
+                # hs = hess(params_opt)
 
-                print(f"Shape of params opt: {params_opt.shape}")
-                print(f"Shape of cfun: {c.shape}")
-                print(f"Shape of jac: {j.shape}")
-                print(f"Shape of hess VP in constraint: {h.shape}")
+                # print(f"Shape of params opt: {params_opt.shape}")
+                # print(f"Shape of cfun: {c.shape}")
+                # print(f"Shape of jac: {j.shape}")
+                # print(f"Shape of hessian: {hs.shape}")
+                # print(f"Shape of hess VP in constraint: {h.shape}")
 
-                n = 2
-                times = []
-                for i in range(n):
-                    start_time = time()
-                    _ = hvp(params_opt, jnp.ones_like(c))
-                    times.append(time() - start_time)
-                    print(time() - start_time)
-                time_avg = sum(times) / n
-                print(f"Hessian VO avg ex time: {time_avg:.4} seconds")
+                # h2 = hvp2(params_opt, jnp.ones_like(c))  # warm start
+                # assert jnp.allclose(h, h2), f"\n{h}\nvs.\n{h2}"
+                # assert
+                # h.shape == (params_opt.size, params_opt.size), f"We want: {(params_opt.size, params_opt.size)}"
+
+                # n = 3
+                # times = []
+                # for i in range(n):
+                #     start_time = time()
+                #     _ = jac(params_opt)
+                #     times.append(time() - start_time)
+                #     print(time() - start_time)
+                # time_avg = sum(times) / n
+                # print(f"Jacobian avg ex time: {time_avg:.4} seconds")
+
+                # times = []
+                # for i in range(n):
+                #     start_time = time()
+                #     _ = hess(params_opt)
+                #     times.append(time() - start_time)
+                #     print(time() - start_time)
+                # time_avg = sum(times) / n
+                # print(f"Hessian avg ex time: {time_avg:.4} seconds")
+
+                # times = []
+                # for i in range(n):
+                #     start_time = time()
+                #     _ = hvp(params_opt, jnp.ones_like(c))
+                #     times.append(time() - start_time)
+                #     print(time() - start_time)
+                # time_avg = sum(times) / n
+                # print(f"Hessian vector product avg ex time: {time_avg:.4} seconds")
 
                 # raise
-
                 cdict["type"] = ctype
                 cdict["fun"] = cfun
                 cdict["jac"] = jac
@@ -160,12 +171,25 @@ class IPOPT(ConstrainedOptimizer, SecondOrderOptimizer):
 
     @staticmethod
     def hvp(x, v, f):
-        h = hessian(f)(x)
-        print(f"Hessian shape: {h.shape}")
+        # h = hessian(f)(x)
+        h = jacfwd(jacfwd(f))(x)
+        # print(f"Hessian shape: {h.shape}")
         return jnp.tensordot(v, h, axes=1)
 
     @staticmethod
+    def hvp(x, v, f):
+        # h = hessian(f)(x)
+        h = jacfwd(jacfwd(f))(x)
+        return jnp.einsum("ijk,i->jk", h, v)
+
+    @staticmethod
+    def hvp4(x, v, f):
+        return jacfwd(jvp(f, (x,), (v, ))[1])
+
+    @staticmethod
     def hvp2(primals, tangents, f):
+        # _, vjp_fun = vjp(grad(f), primals)
+        # return vjp_fun(tangents)
         return jvp(jacfwd(f), (primals, ), (tangents, ))[1]
         # return jvp(grad(f), (primals, ), (tangents, ))[1]
 
