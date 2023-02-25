6842b19eb50ac1061048924a6a9172861a43e0c4
Rafael Pastrana <arpj@princeton.edu>
2023-02-25 11:14:18 -0500

    [Recorders] `OptimizationRecorder.record()` stores history in dict

--- src/jax_fdm/optimization/recorders.py
+++ src/jax_fdm/optimization/recorders.py
@@ -10,17 +10,21 @@ import jax.tree_util as jtu
 # ==========================================================================
 
 class OptimizationRecorder(Data):
+    """A recorder that stores data during the optimization process.
+    """
     def __init__(self, optimizer=None):
         self.optimizer = optimizer
-        self.history = []
-
-    def record(self, value):
-        self.history.append(value)
+        self.history_names = ("q", "xyz_fixed", "loads")
+        self.history = {name: [] for name in self.history_names}
 
     def __call__(self, xk, *args, **kwargs):
         if self.optimizer:
             xk = self.optimizer.parameters_fdm(xk)
         self.record(xk)
+
+    def record(self, parameters):
+        for parameter, name in zip(parameters, self.history_names):
+            self.history[name].append(parameter)
 
     @property
     def data(self):
