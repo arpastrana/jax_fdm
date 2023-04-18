fb1fd7665ee0bc81b663383dfd9990480993a167
Rafael Pastrana <arpj@princeton.edu>
2023-04-18 17:41:58 -0400

    [Equilibrium] Implemented `EquilibriumModel` as an `equinox.Module`

--- src/jax_fdm/equilibrium/model.py
+++ src/jax_fdm/equilibrium/model.py
@@ -1,5 +1,9 @@
 from functools import partial
 
+import jax
+
+import equinox as eqx
+
 import jax.numpy as jnp
 
 from jax_fdm.equilibrium.state import EquilibriumState
@@ -13,78 +17,86 @@ from jax import vmap
 # ==========================================================================
 
 
-class EquilibriumModel:
+class EquilibriumModel(eqx.Module):
     """
     The equilibrium solver.
     """
-    def __init__(self, network):
-        self.structure = EquilibriumStructure(network)
+    q: jax.Array
+    xyz_fixed: jax.Array
+    loads: jax.Array
 
     @classmethod
     def from_network(cls, network):
         """
-        Create an equilibrium model from a force density network.
+        Create an equilibrium model from an force density network.
         """
-        return cls(network)
+        q, xyz_fixed, loads = (jnp.asarray(p) for p in network.parameters())
 
-    def edges_vectors(self, xyz):
+        return cls(q, xyz_fixed, loads)
+
+    @staticmethod
+    def edges_vector(structure, xyz):
         """
         Calculate the unnormalized edge directions.
         """
-        return self.structure.connectivity @ xyz
+        return structure.connectivity @ xyz
 
-    def edges_lengths(self, vectors):
+    @staticmethod
+    def edges_length(vectors):
         """
         Compute the length of the edges.
         """
         return jnp.linalg.norm(vectors, axis=1, keepdims=True)
 
-    def edges_forces(self, q, lengths):
+    @staticmethod
+    def edges_force(q, lengths):
         """
         Calculate the force in the edges.
         """
         return jnp.reshape(q, (-1, 1)) * lengths
 
-    def nodes_residuals(self, q, loads, vectors):
+    @staticmethod
+    def nodes_residual(structure, q, loads, vectors):
         """
         Compute the force at the anchor supports of the structure.
         """
-        connectivity = self.structure.connectivity
+        connectivity = structure.connectivity
         return loads - connectivity.T @ jnp.diag(q) @ vectors
 
-    def nodes_free_positions(self, q, xyz_fixed, loads):
+    @staticmethod
+    def nodes_free_position(structure, q, xyz_fixed, loads):
         """
         Calculate the XYZ coordinates of the free nodes.
         """
         # shorthand
-        free = self.structure.free_nodes
-        c_fixed = self.structure.connectivity_fixed
-        c_free = self.structure.connectivity_free
+        free = structure.free_nodes
+        c_fixed = structure.connectivity_fixed
+        c_free = structure.connectivity_free
 
         A = c_free.T @ vmap(jnp.dot)(q, c_free)
         b = loads[free, :] - c_free.T @ vmap(jnp.dot)(q, c_fixed @ xyz_fixed)
 
         return jnp.linalg.solve(A, b)
 
-    def nodes_positions(self, xyz_free, xyz_fixed):
+    @staticmethod
+    def nodes_position(structure, xyz_free, xyz_fixed):
         """
         Concatenate in order the position of the free and the fixed nodes.
         """
         # NOTE: free fixed indices sorted by enumeration
-        indices = self.structure.freefixed_nodes
+        indices = structure.freefixed_nodes
         return jnp.concatenate((xyz_free, xyz_fixed))[indices, :]
 
-    @partial(jit, static_argnums=0)
-    def __call__(self, q, xyz_fixed, loads):
+    def equilibrium(self, q, xyz_fixed, loads, structure):
         """
-        Compute an equilibrium state using the force density method.
+        Calculate a state of static equilibrium on a structure.
         """
-        xyz_free = self.nodes_free_positions(q, xyz_fixed, loads)
-        xyz_all = self.nodes_positions(xyz_free, xyz_fixed)
-        vectors = self.edges_vectors(xyz_all)
-        residuals = self.nodes_residuals(q, loads, vectors)
-        lengths = self.edges_lengths(vectors)
-        forces = self.edges_forces(q, lengths)
+        xyz_free = self.nodes_free_position(structure, q, xyz_fixed, loads)
+        xyz_all = self.nodes_position(structure, xyz_free, xyz_fixed)
+        vectors = self.edges_vector(structure, xyz_all)
+        residuals = self.nodes_residual(structure, q, loads, vectors)
+        lengths = self.edges_length(vectors)
+        forces = self.edges_force(q, lengths)
 
         return EquilibriumState(xyz=xyz_all,
                                 residuals=residuals,
@@ -93,3 +105,13 @@ class EquilibriumModel:
                                 vectors=vectors,
                                 loads=loads,
                                 force_densities=q)
+
+    def __call__(self, structure):
+        """
+        Compute an equilibrium state using the force density method.
+        """
+        q = self.q
+        xyz_fixed = self.xyz_fixed
+        loads = self.loads
+
+        return self.equilibrium(q, xyz_fixed, loads, structure)
