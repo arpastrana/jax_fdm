62d6bb7c8a34498bf3b3a9b0aa52a56fa1c98cd6
Rafael Pastrana <arpj@princeton.edu>
2023-04-21 10:50:13 -0400

    [Equilibrium] Added some comments to `EquilibriumModel`

--- src/jax_fdm/equilibrium/sparse_solver.py
+++ src/jax_fdm/equilibrium/sparse_solver.py
@@ -8,6 +8,17 @@ import scipy.sparse.linalg
 from jax.experimental.sparse import BCOO, CSC
 
 
+def get_sparse_diag_indices(csr):
+    """Given a CSR matrix, get indices into `data` that access diagonal elements in order."""
+    all_indices = []
+    for i in range(csr.shape[0]):
+        index_range = csr.indices[csr.indptr[i]:csr.indptr[i + 1]]
+        ind_loc = np.where(index_range == i)[0]
+        all_indices.append(ind_loc + csr.indptr[i])
+
+    return np.concatenate(all_indices)
+
+
 def force_densities_to_A(q, index_array, diag_indices, diags):
     """Computes the LHS matrix from a given vector of force densities."""
 
@@ -34,13 +45,20 @@ def linear_solve(q, xyz_fixed, loads, free, c_free, c_fixed, index_array, diag_i
     return xk
 
 def linear_solve_fwd(q, xyz_fixed, loads, free, c_free, c_fixed, index_array, diag_indices, diags):
+    """Forward function of sparse linear solver.
+    Call the linear solve and save parameters and solution for the backward pass."""
     xk = linear_solve(q, xyz_fixed, loads, free, c_free, c_fixed, index_array, diag_indices, diags)
 
     lhs_matrix = force_densities_to_A(q, index_array, diag_indices, diags)
     return xk, (xk, q, xyz_fixed, loads)
 
 def linear_solve_bwd(free, c_free, c_fixed, index_array, diag_indices, diags, res, g):
+    """
+    Backward pass of the sparse linear solver.
+    """
     xk, q, xyz_fixed, loads = res
+
+    # function that translates parameters into lhs matrix in CSC format
     A = force_densities_to_A(q, index_array, diag_indices, diags)
 
     # Solve adjoint system
@@ -49,6 +67,7 @@ def linear_solve_bwd(free, c_free, c_fixed, index_array, diag_indices, diags, re
                             g,  # return type is g
                             A.data, A.indices, A.indptr, g)  # input arguments
 
+    # implicit constraint function
     def residual_fn(params):
         q, xyz_fixed, loads = params
         b = loads[free, :] - c_free.T @ (q[:, None] * (c_fixed @ xyz_fixed))
@@ -60,15 +79,6 @@ def linear_solve_bwd(free, c_free, c_fixed, index_array, diag_indices, diags, re
     params_bar = jax.vjp(residual_fn, p)[1](lam)[0]
     return params_bar
 
+
+
 linear_solve.defvjp(linear_solve_fwd, linear_solve_bwd)
-
-
-def get_sparse_diag_indices(csr):
-    """Given a CSR matrix, get indices into `data` that access diagonal elements in order."""
-    all_indices = []
-    for i in range(csr.shape[0]):
-        index_range = csr.indices[csr.indptr[i]:csr.indptr[i + 1]]
-        ind_loc = np.where(index_range == i)[0]
-        all_indices.append(ind_loc + csr.indptr[i])
-
-    return np.concatenate(all_indices)
