d4bfe7cee91332579576a901921e09ec02d0ac0d
Rafael Pastrana <arpj@princeton.edu>
2023-05-08 16:49:49 -0400

    Bump version to 0.7.1

--- src/jax_fdm/__init__.py
+++ src/jax_fdm/__init__.py
@@ -24,7 +24,7 @@ __author__ = ["Rafael Pastrana"]
 __copyright__ = "Rafael Pastrana"
 __license__ = "MIT License"
 __email__ = "arpastrana@princeton.edu"
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 
 
 HERE = os.path.dirname(__file__)
