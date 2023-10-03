0229159c034bbfbb4a589f9ff819e713474bef59
Rafael Pastrana <arpj@princeton.edu>
2023-10-03 16:37:05 -0400

    [Parameters] Added support for mesh vertex params

--- src/jax_fdm/parameters/parameters.py
+++ src/jax_fdm/parameters/parameters.py
@@ -93,6 +93,23 @@ class NodeParameter(Parameter):
         return network.node_attribute(self.key, name=self.attr_name)
 
 
+class VertexParameter(Parameter):
+    """
+    A vertex parameter.
+    """
+    def index(self, model, structure):
+        """
+        Get the index of the key of the node parameter in the structure of a model.
+        """
+        return structure.vertex_index[self.key]
+
+    def value(self, model, mesh):
+        """
+        Get the current value of the node parameter.
+        """
+        return mesh.node_attribute(self.key, name=self.attr_name)
+
+
 class EdgeParameter(Parameter):
     """
     An edge parameter.
@@ -103,15 +120,15 @@ class EdgeParameter(Parameter):
         """
         return structure.edge_index[self.key]
 
-    def value(self, model, network):
+    def value(self, model, datastructure):
         """
         Get the current value of the edge parameter.
         """
-        return network.edge_attribute(self.key, name=self.attr_name)
+        return datastructure.edge_attribute(self.key, name=self.attr_name)
 
 
 # ==========================================================================
-# ParameterGroups
+# Parameter groups
 # ==========================================================================
 
 class ParameterGroup(Parameter):
@@ -123,7 +140,7 @@ class ParameterGroup(Parameter):
         assert len(self.key) > 0
 
 
-class NodesParameter(ParameterGroup):
+class NodeGroupParameter(ParameterGroup):
     """
     A single parameter applied to a group of nodes.
     """
@@ -141,7 +158,25 @@ class NodesParameter(ParameterGroup):
         return sum(values) / len(values)
 
 
-class EdgesParameter(ParameterGroup):
+class VertexGroupParameter(ParameterGroup):
+    """
+    A single parameter applied to a group of nodes.
+    """
+    def index(self, model, structure):
+        """
+        Get the indices of the keys of the parametrized vertices of a structure.
+        """
+        return [structure.vertex_index[key] for key in self.key]
+
+    def value(self, model, mesh):
+        """
+        Get the current average value of the parameter of the grouped vertices.
+        """
+        values = [mesh.vertex_attribute(key, name=self.attr_name) for key in self.key]
+        return sum(values) / len(values)
+
+
+class EdgeGroupParameter(ParameterGroup):
     """
     A single parameter applied to a group of edges.
     """
@@ -151,11 +186,11 @@ class EdgesParameter(ParameterGroup):
         """
         return [structure.edge_index[key] for key in self.key]
 
-    def value(self, model, network):
+    def value(self, model, datastructure):
         """
         Get the current average value of the parameter of the grouped edges.
         """
-        values = [network.edge_attribute(key, name=self.attr_name) for key in self.key]
+        values = [datastructure.edge_attribute(key, name=self.attr_name) for key in self.key]
         return sum(values) / len(values)
 
 
@@ -172,7 +207,7 @@ class EdgeForceDensityParameter(EdgeParameter):
         self.attr_name = "q"
 
 
-class EdgesForceDensityParameter(EdgesParameter, EdgeForceDensityParameter):
+class EdgeGroupForceDensityParameter(EdgeGroupParameter, EdgeForceDensityParameter):
     """
     A single force density value to rule them all.
     """
@@ -227,7 +262,7 @@ class NodeSupportZParameter(NodeSupportParameter):
 # ==========================================================================
 
 
-class NodesSupportParameter(NodesParameter):
+class NodeGroupSupportParameter(NodeGroupParameter):
     """
     Parametrize a group of support nodes.
     """
@@ -238,7 +273,7 @@ class NodesSupportParameter(NodesParameter):
         return [structure.support_index[key] for key in self.key]
 
 
-class NodesSupportXParameter(NodesSupportParameter, NodeSupportXParameter):
+class NodeGroupSupportXParameter(NodeGroupSupportParameter, NodeSupportXParameter):
     """
     Parametrize wiht a single value the X coordinate of a group of support nodes.
     """
@@ -246,7 +281,7 @@ class NodesSupportXParameter(NodesSupportParameter, NodeSupportXParameter):
         super().__init__(*args, **kwargs)
 
 
-class NodesSupportYParameter(NodesSupportParameter, NodeSupportYParameter):
+class NodeGroupSupportYParameter(NodeGroupSupportParameter, NodeSupportYParameter):
     """
     Parametrize wiht a single value the Y coordinate of a group of support nodes.
     """
@@ -254,7 +289,7 @@ class NodesSupportYParameter(NodesSupportParameter, NodeSupportYParameter):
         super().__init__(*args, **kwargs)
 
 
-class NodesSupportZParameter(NodesSupportParameter, NodeSupportZParameter):
+class NodeGroupSupportZParameter(NodeGroupSupportParameter, NodeSupportZParameter):
     """
     Parametrize wiht a single value the Z coordinate of a group of support nodes.
     """
@@ -301,7 +336,7 @@ class NodeLoadZParameter(NodeLoadParameter):
 # Node loads groups
 # ==========================================================================
 
-class NodesLoadXParameter(NodesParameter, NodeLoadXParameter):
+class NodeGroupLoadXParameter(NodeGroupParameter, NodeLoadXParameter):
     """
     Parametrize wiht a single value the X component of the load applied to a group of nodes.
     """
@@ -309,7 +344,7 @@ class NodesLoadXParameter(NodesParameter, NodeLoadXParameter):
         super().__init__(*args, **kwargs)
 
 
-class NodesLoadYParameter(NodesParameter, NodeLoadXParameter):
+class NodeGroupLoadYParameter(NodeGroupParameter, NodeLoadXParameter):
     """
     Parametrize wiht a single value the Y component of the load applied to a group of nodes.
     """
@@ -317,7 +352,140 @@ class NodesLoadYParameter(NodesParameter, NodeLoadXParameter):
         super().__init__(*args, **kwargs)
 
 
-class NodesLoadZParameter(NodesParameter, NodeLoadZParameter):
+class NodeGroupLoadZParameter(NodeGroupParameter, NodeLoadZParameter):
+    """
+    Parametrize wiht a single value the Z component of the load applied to a group of nodes.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+# ==========================================================================
+# Vertex supports
+# ==========================================================================
+
+class VertexSupportParameter(VertexParameter):
+    """
+    A node support parameter.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class VertexSupportXParameter(VertexSupportParameter, NodeSupportXParameter):
+    """
+    Parametrize the X coordinate of a support node.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class VertexSupportYParameter(VertexSupportParameter, NodeSupportYParameter):
+    """
+    Parametrize the Y coordinate of a support node.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class VertexSupportZParameter(VertexSupportParameter, NodeSupportZParameter):
+    """
+    Parametrize the Z coordinate of a support node.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+# ==========================================================================
+# Vertex supports groups
+# ==========================================================================
+
+
+class VertexGroupSupportParameter(NodeGroupParameter):
+    """
+    Parametrize a group of support nodes.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class VertexGroupSupportXParameter(VertexGroupParameter, VertexSupportXParameter):
+    """
+    Parametrize wiht a single value the X coordinate of a group of support nodes.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class VertexGroupSupportYParameter(VertexGroupParameter, VertexSupportYParameter):
+    """
+    Parametrize wiht a single value the Y coordinate of a group of support nodes.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class VertexGroupSupportZParameter(VertexGroupParameter, VertexSupportZParameter):
+    """
+    Parametrize wiht a single value the Z coordinate of a group of support nodes.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+# ==========================================================================
+# Vertex loads
+# ==========================================================================
+
+class VertexLoadParameter(VertexParameter):
+    pass
+
+
+class VertexLoadXParameter(VertexLoadParameter, NodeLoadXParameter):
+    """
+    Parametrize the x component of a node load.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class VertexLoadYParameter(VertexLoadParameter, NodeLoadYParameter):
+    """
+    Parametrize the y component of a node load.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class VertexLoadZParameter(VertexLoadParameter, NodeLoadZParameter):
+    """
+    Parametrize the z component of a node load.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+# ==========================================================================
+# Vertex loads groups
+# ==========================================================================
+
+class VertexGroupLoadXParameter(VertexGroupParameter, VertexLoadXParameter):
+    """
+    Parametrize wiht a single value the X component of the load applied to a group of nodes.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class VertesGroupLoadYParameter(VertexGroupParameter, VertexLoadXParameter):
+    """
+    Parametrize wiht a single value the Y component of the load applied to a group of nodes.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class VertexGroupLoadZParameter(VertexGroupParameter, VertexLoadZParameter):
     """
     Parametrize wiht a single value the Z component of the load applied to a group of nodes.
     """
