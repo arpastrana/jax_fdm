13fe049d32836e1adc2933135b90806d7ccba658
Rafael Pastrana <arpj@princeton.edu>
2024-05-09 10:20:25 -0400

    Bump version to 0.8.4

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="jax_fdm",
-    version="0.8.3",
+    version="0.8.4",
     description="Auto-differentiable and hardware-accelerated force density method",
     long_description=long_description,
     long_description_content_type="text/markdown",
