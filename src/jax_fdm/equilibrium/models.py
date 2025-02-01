bf37028c5e0f86c903c7e8ca3b5c9ff08bd1b511
Rafael Pastrana <arpj@princeton.edu>
2025-02-01 12:36:05 -0500

    [Eq] `EquilibriumModel.equilibrium()` and `EquilibriumModel.equilibrium_iterative()` return `xyz_free`

--- src/jax_fdm/equilibrium/models.py
+++ src/jax_fdm/equilibrium/models.py
@@ -25,24 +25,36 @@ from jax_fdm.equilibrium.states import LoadState
 
 class EquilibriumModel:
     """
-    The equilibrium model.
+    A FDM model to calculate equilibrium states with shape-dependent loads.
+
+    Parameters
+    ----------
+    `tmax`: The maximum number of iterations to calculate an equilibrium state. If `tmax=1`, the model is equivalent to doing one linear FDM step, and the rest of the parameters of this model are ignored. The edge and face loads are discarded too. Defaults to `100`.
+    `eta`: The convergence tolerance for calculating an equilibrium state. Defaults to `1e-6`.
+    `is_load_local`: If set to `True`, the face and edge loads are applied in their local coordinate system at every iteration (follower loads). Defaults to `False`.
+    `minimize_residual`: If set to `True`, it explicitly minimizes a residual function with `itersolve_fn` per iteration. Otherwise, the model performs a fixed-point iteration on the shape of a structure. Defaults to `False`.
+    `itersolve_fn`: The function that calculates an equilibrium state iteratively. If `None`, the model defaults to forward fixed-point iteration. Note that only the solver must be consistent with the choice of residual function. Defaults to `None`.
+    `implicit_diff`: If set to `True`, it applies implicit differentiation to speed up backpropagation. Defaults to `True`.
+    `ignore_nodes_load`: Whether to only apply edge and face loads during the iterative equilibrium calculations. Defaults to `True`.
+    `verbose`: Whether to print out calculations' info to the terminal. Defaults to `False`.
     """
     def __init__(self,
                  tmax=100,
                  eta=1e-6,
                  is_load_local=False,
+                 minimize_residual=True,
                  itersolve_fn=None,
                  implicit_diff=True,
-                 load_nodes_iter=False,
+                 ignore_nodes_load=True,
                  verbose=False):
-
         self.tmax = tmax
         self.eta = eta
         self.is_load_local = is_load_local
+        self.minimize_residual = minimize_residual
         self.linearsolve_fn = jnp.linalg.solve
         self.itersolve_fn = itersolve_fn or solver_forward
         self.implicit_diff = implicit_diff
-        self.load_nodes_iter = load_nodes_iter
+        self.ignore_nodes_load = ignore_nodes_load
         self.verbose = verbose
 
     # ----------------------------------------------------------------------
@@ -82,10 +94,12 @@ class EquilibriumModel:
         return loads - connectivity.T @ (q[:, None] * vectors)
 
     @staticmethod
-    def nodes_positions(xyz_free, xyz_fixed, indices):
+    def nodes_positions(xyz_free, xyz_fixed, structure):
         """
         Concatenate in order the position of the free and the fixed nodes.
         """
+        indices = structure.indices_freefixed
+
         return jnp.concatenate((xyz_free, xyz_fixed))[indices, :]
 
     def nodes_free_positions(self, q, xyz_fixed, loads, structure):
@@ -101,10 +115,7 @@ class EquilibriumModel:
         """
         Calculate static equilibrium on the nodes of a structure.
         """
-        xyz_free = self.nodes_free_positions(q, xyz_fixed, loads_nodes, structure)
-        indices = structure.indices_freefixed
-
-        return self.nodes_positions(xyz_free, xyz_fixed, indices)
+        return self.nodes_free_positions(q, xyz_fixed, loads_nodes, structure)
 
     # --------------------------------------------------------------------
     #  Load it up!
@@ -159,23 +170,27 @@ class EquilibriumModel:
         implicit_diff = self.implicit_diff
         verbose = self.verbose
 
-        xyz = self.equilibrium(q, xyz_fixed, loads_nodes, structure)
+        xyz_free = self.equilibrium(q, xyz_fixed, loads_nodes, structure)
 
         if tmax > 1:
+
             # Setting node loads to zero when tmax > 1 if specified
-            if not self.load_nodes_iter:
+            if self.ignore_nodes_load:
                 loads_nodes = jnp.zeros_like(loads_nodes)
                 loads_state = LoadState(loads_nodes,
                                         loads_state.edges,
                                         loads_state.faces)
 
-            # xyz = self.equilibrium_iterative(
-            xyz = self.equilibrium_iterative_residual(
+            equilibrium_iterative_fn = self.equilibrium_iterative_residual
+            if not self.minimize_residual:
+                equilibrium_iterative_fn = self.equilibrium_iterative_xyz
+
+            xyz_free = equilibrium_iterative_fn(
                 q,
                 xyz_fixed,
                 loads_state,
                 structure,
-                xyz_init=None,  # xyz,
+                xyz_free_init=xyz_free,
                 tmax=tmax,
                 eta=eta,
                 solver=solver,
@@ -183,6 +198,8 @@ class EquilibriumModel:
                 verbose=verbose
             )
 
+        # Exit like a champ
+        xyz = self.nodes_positions(xyz_free, xyz_fixed, structure)
         loads_nodes = self.nodes_load(xyz, loads_state, structure)
 
         return self.equilibrium_state(q, xyz, loads_nodes, structure)
@@ -197,17 +214,18 @@ class EquilibriumModel:
         """
         return self.nodes_equilibrium(q, xyz_fixed, loads_nodes, structure)
 
-    def equilibrium_iterative(self,
-                              q,
-                              xyz_fixed,
-                              load_state,
-                              structure,
-                              xyz_init=None,
-                              tmax=100,
-                              eta=1e-6,
-                              solver=None,
-                              implicit_diff=True,
-                              verbose=False):
+    def equilibrium_iterative_xyz(
+            self,
+            q,
+            xyz_fixed,
+            load_state,
+            structure,
+            xyz_free_init=None,
+            tmax=100,
+            eta=1e-6,
+            solver=None,
+            implicit_diff=True,
+            verbose=False):
         """
         Calculate static equilibrium on a structure iteratively.
 
@@ -216,7 +234,7 @@ class EquilibriumModel:
         This function only supports reverse mode auto-differentiation.
         To support forward-mode, we should define a custom jvp using implicit differentiation.
         """
-        def equilibrium_iterative_fn(params, xyz_init):
+        def equilibrium_iterative_fn(params, xyz_free):
             """
             This closure function avoids re-computing A and f_fixed throughout iterations
             because these two matrices remain constant during the fixed point search.
@@ -224,19 +242,15 @@ class EquilibriumModel:
             # TODO: Extract closure into function shared with the other nodes equilibrium function?
             A, f_fixed, xyz_fixed, load_state = params
 
-            free = structure.indices_free
-            freefixed = structure.indices_freefixed
+            xyz = self.nodes_positions(xyz_free, xyz_fixed, structure)
+            loads_nodes = self.nodes_load(xyz, load_state, structure)
+            b = loads_nodes[structure.indices_free, :] - f_fixed
 
-            loads_nodes = self.nodes_load(xyz_init, load_state, structure)
-            b = loads_nodes[free, :] - f_fixed
-            xyz_free = self.linearsolve_fn(A, b)
-            xyz_ = self.nodes_positions(xyz_free, xyz_fixed, freefixed)
-
-            return xyz_
+            return self.linearsolve_fn(A, b)
 
         # recompute xyz_init if not input
-        if xyz_init is None:
-            xyz_init = self.equilibrium(q, xyz_fixed, load_state.nodes, structure)
+        if xyz_free_init is None:
+            xyz_free_init = self.equilibrium(q, xyz_fixed, load_state.nodes, structure)
 
         A = self.stiffness_matrix(q, structure)
         f_fixed = self.force_fixed_matrix(q, xyz_fixed, structure)
@@ -250,7 +264,7 @@ class EquilibriumModel:
         solver_kwargs = {"solver_config": solver_config,
                          "f": equilibrium_iterative_fn,
                          "a": (A, f_fixed, xyz_fixed, load_state),
-                         "x_init": xyz_init}
+                         "x_init": xyz_free_init}
 
         if implicit_diff:
             return fixed_point(solver, **solver_kwargs)
@@ -262,7 +276,7 @@ class EquilibriumModel:
                                        xyz_fixed,
                                        load_state,
                                        structure,
-                                       xyz_init=None,
+                                       xyz_free_init=None,
                                        tmax=100,
                                        eta=1e-6,
                                        solver=None,
@@ -284,7 +298,7 @@ class EquilibriumModel:
             q, xyz_fixed, load_state = params
 
             # Concatenate free and fixed xyz coordinates
-            xyz = self.nodes_positions(xyz_free, xyz_fixed, structure.indices_freefixed)
+            xyz = self.nodes_positions(xyz_free, xyz_fixed, structure)
 
             # Calculate shape-dependent loads with full xyz
             loads_nodes = self.nodes_load(xyz, load_state, structure)
@@ -310,16 +324,18 @@ class EquilibriumModel:
 
             return residual.ravel()
 
-        # recompute xyz_init if not input
-        if xyz_init is None:
-            print("Calculating initial guess")
-            xyz_free_init = self.nodes_free_positions(q, xyz_fixed, load_state.nodes, structure)
-            xyz_free_init = xyz_free_init.ravel()
+        # Recompute xyz_free_init if not input
+        if xyz_free_init is None:
+            load_nodes = load_state.nodes
+            xyz_free_init = self.nodes_free_positions(q, xyz_fixed, load_nodes, structure)
 
-        # Params
+        # Flatten XYZ free for compatibility with the loss function
+        xyz_free_init = xyz_free_init.ravel()
+
+        # Parameterss
         params = (q, xyz_fixed, load_state)
 
-        solver = solver or self.itersolve_fn
+        # Solver
         solver_config = {"tmax": tmax,
                          "eta": eta,
                          "implicit_diff": implicit_diff,
@@ -330,6 +346,7 @@ class EquilibriumModel:
                          "a": params,
                          "x_init": xyz_free_init}
 
+        solver = solver or self.itersolve_fn
         # if implicit_diff:
         #    xyz_free_star = fixed_point(solver, **solver_kwargs)
         # else:
@@ -341,9 +358,8 @@ class EquilibriumModel:
         print(f"Mean solution residual: {jnp.mean(residual).item():.3f}")
 
         xyz_free_star = jnp.reshape(xyz_free_star, (-1, 3))
-        xyz = self.nodes_positions(xyz_free_star, xyz_fixed, structure.indices_freefixed)
 
-        return xyz
+        return xyz_free_star
 
     # ----------------------------------------------------------------------
     # Equilibrium state
@@ -397,19 +413,6 @@ class EquilibriumModel:
         c_fixed = structure.connectivity_fixed
 
         return c_free.T @ (q[:, None] * (c_fixed @ xyz_fixed))
-
-    def force_matrix_shape_dependent(self, xyz_free, q, xyz_fixed, load_state, structure):
-        """
-        The force residual matrix of the structure.
-        """
-        # Concatenate free and fixed xyz coordinates
-        xyz = self.nodes_positions(xyz_free, xyz_fixed, structure.indices_freefixed)
-
-        # Calculate shape-dependent loads with full xyz
-        loads_nodes = self.nodes_load(xyz, load_state, structure)
-
-        # Return load matrix
-        return self.force_matrix(q, xyz_fixed, loads_nodes, structure)
 
 
 # ==========================================================================
