8b319f686753016bcca5482fbbd59ba7f40e9b8c
Rafael Pastrana <arpj@princeton.edu>
2025-02-06 11:20:30 -0500

    [Solvers] Wrapped up more `optimistix` solvers

--- src/jax_fdm/equilibrium/solvers/__init__.py
+++ src/jax_fdm/equilibrium/solvers/__init__.py
@@ -1,5 +1,5 @@
 from .jaxopt import *  # noqa F403
-# from .optimistix import *  # noqa F403
+from .optimistix import *  # noqa F403
 from .fixed_point import *  # noqa F403
 from .least_squares import *  # noqa F403
 
@@ -11,6 +11,11 @@ SOLVERS = {
     "levenberg_marquardt": solver_levenberg_marquardt,
     "lbfgs": solver_lbfgs,
     "lbfgs_scipy": solver_lbfgs_scipy,
+    "levenberg_marquardt_optimistix": solver_levenberg_marquardt_optimistix,
+    "dogleg_optimistix": solver_levenberg_marquardt_optimistix,
+    "bfgs_optimistix": solver_bfgs_optimistix,
+    "nonlinear_cg_optimistix": solver_nonlinear_cg_optimistix,
+    "newton_optimistix": solver_newton_optimistix,
 }
 
 __all__ = [name for name in dir() if not name.startswith('_')]
--- src/jax_fdm/equilibrium/solvers/optimistix.py
+++ src/jax_fdm/equilibrium/solvers/optimistix.py
@@ -1,13 +1,104 @@
-from optimistix import LevenbergMarquardt
-from optimistix import least_squares
+from jax.tree_util import Partial
 
-from optimistix import ImplicitAdjoint
-from optimistix import RecursiveCheckpointAdjoint
+try:
+    from optimistix import Newton
+    from optimistix import NonlinearCG
+    from optimistix import BFGS
+    from optimistix import Dogleg
+    from optimistix import LevenbergMarquardt
+    from optimistix import ImplicitAdjoint
+    from optimistix import RecursiveCheckpointAdjoint
+    from optimistix import two_norm
+    from optimistix import least_squares
+    from optimistix import minimise
+    from optimistix import root_find
+
+    import lineax as lx
+
+except ImportError:
+    pass
 
 
-def solver_levenberg_marquardt_optimistix(fn, theta, x_init, solver_config):
+def is_solver_minimization(solver):
     """
-    Solve for a fixed point of a function f(a, x) using anderson acceleration in jaxopt.
+    Test if a solver function is a minimization solver.
+
+    Parameters
+    ----------
+    `solver_fn`: A solver function
+
+    Returns
+    -------
+    `True` if the solver is a least squares solver. Otherwise, `False`.
+    """
+    solvers = {
+        solver_bfgs_optimistix,
+        solver_nonlinear_cg_optimistix,
+    }
+
+    return solver in solvers
+
+
+def is_solver_root_finding(solver):
+    """
+    Test if a solver function is a root finding solver.
+
+    Parameters
+    ----------
+    `solver_fn`: A solver function
+
+    Returns
+    -------
+    `True` if the solver is a least squares solver. Otherwise, `False`.
+    """
+    solvers = {
+        solver_newton_optimistix,
+    }
+
+    return solver in solvers
+
+
+def solver_levenberg_marquardt_optimistix(fn, solver_config):
+    """
+    """
+    eta = solver_config["eta"]
+    solver_kwargs = {"linear_solver": lx.NormalCG(eta, eta)}
+    return solver_optimistix(LevenbergMarquardt, lsq_fn, fn, solver_config, solver_kwargs)
+
+
+def solver_dogleg_optimistix(fn, solver_config):
+    """
+    """
+    eta = solver_config["eta"]
+    solver_kwargs = {"linear_solver": lx.NormalCG(eta, eta)}
+    return solver_optimistix(Dogleg, lsq_fn, fn, solver_config, solver_kwargs)
+
+
+def solver_bfgs_optimistix(fn, solver_config):
+    """
+    """
+    solver_kwargs = {"use_inverse": True}
+    return solver_optimistix(BFGS, min_fn, fn, solver_config, solver_kwargs)
+
+
+def solver_nonlinear_cg_optimistix(fn, solver_config):
+    """
+    """
+    solver_kwargs = {}
+    return solver_optimistix(NonlinearCG, min_fn, fn, solver_config, solver_kwargs)
+
+
+def solver_newton_optimistix(fn, solver_config):
+    """
+    """
+    eta = solver_config["eta"]
+    solver_kwargs = {}
+    return solver_optimistix(Newton, root_fn, fn, solver_config, solver_kwargs)
+
+
+def solver_optimistix(solver_cls, routine_fn, fn, solver_config, solver_kwargs=None):
+    """
+    Find a root of a function f(a, x) with optimistix.
 
     Parameters
     ----------
@@ -31,28 +122,82 @@ def solver_levenberg_marquardt_optimistix(fn, theta, x_init, solver_config):
     else:
         adjoint = RecursiveCheckpointAdjoint()
 
-    def fn_swapped(x, theta):
-        return fn(theta, x)
+    if solver_kwargs is None:
+        solver_kwargs = {}
 
-    stats = {}
-    if verbose:
+    verbose_free = {NonlinearCG, Newton}
+    if verbose and solver_cls not in verbose_free:
         stats = {"loss", "step_size"}
+        verbose = frozenset(stats)
+        solver_kwargs["verbose"] = verbose
 
-    verbose = frozenset(stats)
-    solver = LevenbergMarquardt(
+    solver = solver_cls(
         rtol=eta,
         atol=eta,
-        verbose=verbose,
+        **solver_kwargs,
     )
 
+    return Partial(routine_fn, solver=solver, tmax=tmax, adjoint=adjoint, fn=fn)
+
+
+def lsq_fn(solver, fn, tmax, adjoint, x_init, theta, structure):
+    """
+    """
+    # NOTE: Creating a lambda or a function closure triggers recompilation!
+    # But using jtu.Partial fixes that issue.
+    _fn = Partial(fn, structure=structure)
+
     solution = least_squares(
-        fn=fn_swapped,
+        fn=_fn,
         solver=solver,
         y0=x_init,
         args=theta,
         has_aux=False,
         max_steps=tmax,
         throw=False,
+        adjoint=adjoint
+    )
+
+    return solution.value
+
+
+def min_fn(solver, fn, tmax, adjoint, x_init, theta, structure):
+    """
+    """
+    # NOTE: Creating a lambda or a function closure triggers recompilation!
+    # But using jtu.Partial fixes that issue.
+    _fn = Partial(fn, structure=structure)
+
+    solution = minimise(
+        fn=_fn,
+        solver=solver,
+        y0=x_init,
+        args=theta,
+        has_aux=False,
+        max_steps=tmax,
+        throw=False,
+        adjoint=adjoint
+    )
+
+    return solution.value
+
+
+def root_fn(solver, fn, tmax, adjoint, x_init, theta, structure):
+    """
+    """
+    # NOTE: Creating a lambda or a function closure triggers recompilation!
+    # But using jtu.Partial fixes that issue.
+    _fn = Partial(fn, structure=structure)
+
+    solution = root_find(
+        fn=_fn,
+        solver=solver,
+        y0=x_init,
+        args=theta,
+        has_aux=False,
+        max_steps=tmax,
+        throw=False,
+        adjoint=adjoint
     )
 
     return solution.value
